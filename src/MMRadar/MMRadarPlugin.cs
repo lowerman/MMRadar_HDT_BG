84fe429188df96c871e38d56f22bc8e4fe6a0e1e
lowerman <lowerman@users.noreply.github.com>
2026-07-20 21:31:33 +0300

    Release 1.0.8: rating trust package and censored-network resilience

--- src/MMRadar/MMRadarPlugin.cs
+++ src/MMRadar/MMRadarPlugin.cs
@@ -22,7 +22,7 @@ namespace MMRadar
 
         public string Author => "lowerman";
 
-        public Version Version => new Version(1, 0, 7);
+        public Version Version => new Version(1, 0, 8);
 
         public MenuItem MenuItem => _menuItem;
 
